7289a076dcb30e66ebb26e779d77a5917d3db8bf
Aren <106656925+meltyblend@users.noreply.github.com>
2025-02-22 15:23:30 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,8 @@
 from flask import (Flask, render_template,
                    request, jsonify, abort)
-from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
-import os
 
 load_dotenv()
 app = Flask(__name__)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
-
-db = SQLAlchemy()
-
-db.init_app(app)
-
-
 
 @app.route('/')
 def hello():
