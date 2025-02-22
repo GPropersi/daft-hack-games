9ae276a566de581a20409b8e1aab8bb478dd84aa
Mikey <81997448+EggyPlant@users.noreply.github.com>
2025-02-22 15:10:07 -0800

    add_categories WIP (#11)

---
+++ add_categories.py
@@ -0,0 +1,19 @@
+from app import app, db
+from model import Users, UserStats, Category
+from strings import CATEGORIES
+
+def add_categories():
+
+  with app.app_context():
+    for category_name in CATEGORIES:
+      if db.session.execute(db.select(Category).filter_by(name=category_name)).scalar_one_or_none() is not None: continue
+      category = Category()
+      category.name = category_name 
+
+      db.session.add(category)
+      db.session.commit()
+
+
+
+if __name__ == "__main__":
+  add_categories()
--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import (Flask, render_template,
                    request, jsonify, abort)
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+
+db = SQLAlchemy()
+
+db.init_app(app)
+
+
 
 @app.route('/')
 def hello():
